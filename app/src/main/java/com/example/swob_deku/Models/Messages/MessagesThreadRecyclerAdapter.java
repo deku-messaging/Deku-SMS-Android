688a6c5a9a3bf5519d3878b3dffde2c7a6c47ee4
sherlock <wisdomnji@gmail.com>
2023-03-24 17:10:52 +0100

    update: themed threaded items

--- app/src/main/java/com/example/swob_deku/Models/Messages/MessagesThreadRecyclerAdapter.java
+++ app/src/main/java/com/example/swob_deku/Models/Messages/MessagesThreadRecyclerAdapter.java
@@ -216,16 +216,12 @@ public class MessagesThreadRecyclerAdapter extends RecyclerView.Adapter<Messages
             holder.routingURLText.setVisibility(View.GONE);
         }
 
-//        // TODO: change color of unread messages in thread
 //        if(SMSHandler.hasUnreadMessages(context, smsThreadId)) {
-//            // Make bold
-//            holder.address.setTypeface(null, Typeface.BOLD);
-//            holder.snippet.setTypeface(null, Typeface.BOLD);
-//
-//            holder.address.setTextColor(context.getResources().getColor(R.color.read_text));
-//            holder.snippet.setTextColor(context.getResources().getColor(R.color.read_text));
-//            holder.date.setTextColor(context.getResources().getColor(R.color.read_text));
-//        }
+        if(sms.isRead() == 0) {
+            holder.contactPhoto.setIndicatorEnabled(true);
+            holder.address.setTypeface(Typeface.DEFAULT_BOLD);
+            holder.snippet.setTypeface(Typeface.DEFAULT_BOLD);
+        }
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
