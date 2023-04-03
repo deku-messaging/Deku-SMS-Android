a3c91232af0d6e535cf272d7cc5b82aa6487cd5f
sherlock <wisdomnji@gmail.com>
2023-04-03 16:40:41 +0100

    update: temporal data action removal

--- app/src/main/java/com/example/swob_deku/BroadcastSMSDataActivity.java
+++ app/src/main/java/com/example/swob_deku/BroadcastSMSDataActivity.java
@@ -28,58 +28,59 @@ public class BroadcastSMSDataActivity extends BroadcastReceiver {
             Log.d(getClass().getName(), "New data received..");
 
         if (intent.getAction().equals(Telephony.Sms.Intents.DATA_SMS_RECEIVED_ACTION)) {
-            switch (getResultCode()) {
-                case Activity.RESULT_OK:
-                    ByteArrayOutputStream messageBuffer = new ByteArrayOutputStream();
-                    String address = new String();
-
-                    for (SmsMessage currentSMS : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
-                        address = currentSMS.getDisplayOriginatingAddress();
-
-                        try {
-                            messageBuffer.write(currentSMS.getUserData());
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-
-                    try {
-                        Log.d(getClass().getName(), "Date image 0: " +
-                                Byte.toUnsignedInt(messageBuffer.toByteArray()[0]));
-
-                        Log.d(getClass().getName(), "Date image 1: " +
-                                Byte.toUnsignedInt(messageBuffer.toByteArray()[1]));
-
-                        String metaHeaderInformation = ImageHandler.IMAGE_HEADER +
-                                ImageHandler.getImageMetaRIL(messageBuffer.toByteArray());
-
-                        String strMessage = metaHeaderInformation +
-                                Base64.encodeToString(messageBuffer.toByteArray(), Base64.DEFAULT);
-
-                        Log.d(getClass().getName(), "Data image data: " + strMessage);
-
-                        SMSHandler.registerIncomingMessage(context, address, strMessage);
-
-                        String defaultRIL = Byte.toUnsignedInt(messageBuffer.toByteArray()[0]) + "0";
-
-                        if(ImageHandler.isImageBody(messageBuffer.toByteArray())) {
-                            Log.d(getClass().getName(), "Data image body found");
-
-                            boolean canComposeImage = ImageHandler.canComposeImage(context, defaultRIL);
-                            if(canComposeImage) {
-                                long messageId = ImageHandler.composeAppendDeleteImages(context, defaultRIL,
-                                        Byte.toUnsignedInt(messageBuffer.toByteArray()[0]));
-
-                                String notificationNote = "New image data!";
-                                BroadcastSMSTextActivity.sendNotification(context, notificationNote, address, messageId);
-                            }
-                        }
-
-                    }catch(Exception e ) {
-                        e.printStackTrace();
-                    }
-                    break;
-            }
+//
+//            switch (getResultCode()) {
+//                case Activity.RESULT_OK:
+//                    ByteArrayOutputStream messageBuffer = new ByteArrayOutputStream();
+//                    String address = new String();
+//
+//                    for (SmsMessage currentSMS : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+//                        address = currentSMS.getDisplayOriginatingAddress();
+//
+//                        try {
+//                            messageBuffer.write(currentSMS.getUserData());
+//                        } catch (IOException e) {
+//                            throw new RuntimeException(e);
+//                        }
+//                    }
+//
+//                    try {
+//                        Log.d(getClass().getName(), "Date image 0: " +
+//                                Byte.toUnsignedInt(messageBuffer.toByteArray()[0]));
+//
+//                        Log.d(getClass().getName(), "Date image 1: " +
+//                                Byte.toUnsignedInt(messageBuffer.toByteArray()[1]));
+//
+//                        String metaHeaderInformation = ImageHandler.IMAGE_HEADER +
+//                                ImageHandler.getImageMetaRIL(messageBuffer.toByteArray());
+//
+//                        String strMessage = metaHeaderInformation +
+//                                Base64.encodeToString(messageBuffer.toByteArray(), Base64.DEFAULT);
+//
+//                        Log.d(getClass().getName(), "Data image data: " + strMessage);
+//
+//                        SMSHandler.registerIncomingMessage(context, address, strMessage);
+//
+//                        String defaultRIL = Byte.toUnsignedInt(messageBuffer.toByteArray()[0]) + "0";
+//
+//                        if(ImageHandler.isImageBody(messageBuffer.toByteArray())) {
+//                            Log.d(getClass().getName(), "Data image body found");
+//
+//                            boolean canComposeImage = ImageHandler.canComposeImage(context, defaultRIL);
+//                            if(canComposeImage) {
+//                                long messageId = ImageHandler.composeAppendDeleteImages(context, defaultRIL,
+//                                        Byte.toUnsignedInt(messageBuffer.toByteArray()[0]));
+//
+//                                String notificationNote = "New image data!";
+//                                BroadcastSMSTextActivity.sendNotification(context, notificationNote, address, messageId);
+//                            }
+//                        }
+//
+//                    }catch(Exception e ) {
+//                        e.printStackTrace();
+//                    }
+//                    break;
+//            }
         }
     }
 }
