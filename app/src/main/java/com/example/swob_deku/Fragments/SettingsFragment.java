27873e3f5ded4fe758acee9b427fc4fc6aea9450
sherlock wisdom <wisdomnji@gmail.com>
2023-05-19 00:21:35 +0100

    update: work required for settings preferences

---
+++ app/src/main/java/com/example/swob_deku/Fragments/SettingsFragment.java
@@ -0,0 +1,27 @@
+package com.example.swob_deku.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.swob_deku.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+        setPreferencesFromResource(R.xml.settings_preferences, rootKey);
+
+        Preference securityPrivacyPreference = findPreference("security_id_end_to_end_encryption");
+        securityPrivacyPreference.setFragment(EndToEndFragments.class.getCanonicalName());
+    }
+}
--- app/src/main/java/com/example/swob_deku/SettingsActivity.java
+++ app/src/main/java/com/example/swob_deku/SettingsActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.swob_deku.Fragments.SettingsFragment;
+
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
