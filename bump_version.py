b8576f578cad3a2d69af3fd2d8a97f2e838dcc33
sherlock wisdom <wisdomnji@gmail.com>
2023-08-15 09:41:57 +0100

    release: making release

--- bump_version.py
+++ bump_version.py
@@ -1,12 +1,11 @@
 import os
 
-def bump_version(filename, version): 
+def bump_version(filename):
     """
     Bumps the version number in the specified file.
 
     Args:
     filename: The name of the file to update.
-    version: The current version number.
 
     Returns:
     The new version number.
@@ -15,19 +14,26 @@ def bump_version(filename, version):
     with open(filename, "r") as f:
         lines = f.readlines()
 
-    for i, line in enumerate(lines):
-        if line.startswith("releaseVersion="):
-            lines[i] = "releaseVersion=" + str(version)
+    version = None
+    for line in lines:
+        if line.startswith("releaseVersion="): 
+            version = line.split("=")[1].strip() 
+            break
+
+    if version is None:
+        raise ValueError("Could not find releaseVersion in file")
+
+    new_version = int(version) + 1
 
     with open(filename, "w") as f:
-        f.writelines(lines)
+        f.write("releaseVersion=" + str(new_version))
 
-    return version + 1
+    return new_version
+
 
 if __name__ == "__main__":
   filename = "version.properties"
-  version = 24
-  new_version = bump_version(filename, version)
+  new_version = bump_version(filename)
 
   print("Successfully bumped version to {}".format(new_version))
 
