19234107b678610290bf70586fc7f6ba6592ba08
sherlock wisdom <wisdomnji@gmail.com>
2023-09-03 19:27:39 +0100

    update: added release for playstore

--- release.py
+++ release.py
@@ -4,70 +4,137 @@ import requests
 import json
 import sys
 
-def create_release(version, title, description, target_branch):
-    """Create a new release on GitHub."""
-    url = "https://api.github.com/repos/deku-messaging/Deku-SMS-Android/releases"
-    data = {
-        "tag_name": version,
-        "name": title,
-        "body": description,
-        "target_commitish": target_branch,
-        "draft": False,
-        "prerelease":False,
-        "generate_release_notes":False
-    }
+from googleapiclient.discovery import build
+# from google.oauth2.credentials import Credentials 
+from google.oauth2 import service_account 
 
-    github_token = get_github_token()
+class RelGooglePlaystore:
 
-    print(github_token)
+    def create_edit_for_draft_release(self, credentials_file_path, release_id, edit_body, bundle_file): 
+        package_name = "com.afkanerd.deku"
+        track = 'internal'
+        release_name = '0.0.1'
 
-    headers = {"Authorization": "Bearer {}".format(github_token), 
-               "X-GitHub-Api-Version": "2022-11-28",
-               "Accept": "application/vnd.github+json"}
+        credentials = service_account.Credentials.from_service_account_file(credentials_file_path)
+        service = build('androidpublisher', 'v3', credentials=credentials)
 
-    response = requests.post(url, json=data, headers=headers)
-    response.raise_for_status()
+        # return service.edits().insert(editId=release_id, body=edit_body).execute()
+        edit_request = service.edits().insert(packageName=package_name)
+        edit_response = edit_request.execute()
+        edit_id = edit_response['id']
 
-    print(response.status_code)
-    return json.loads(response.text)
+        bundle_response = service.edits().bundles().upload(
+                packageName=package_name, 
+                editId=edit_id, 
+                media_mime_type="application/octet-stream",
+                media_body=bundle_file,).execute()
 
-def get_github_token():
-    github_token = None
-    with open('release.properties', 'r') as fd:
-        lines = fd.readlines()
+        bundle_version_code = bundle_response['versionCode']
 
-    for line in lines:
-        if line.startswith('github_token'):
-            github_token = line.split("=")[1].strip() 
-            return github_token
+        # Specify the version code for the draft release
+        version_code = bundle_version_code  # Use the version code of the uploaded bundle
+
+        # Update the track to create a draft release
+        track_request = service.edits().tracks().update(
+            packageName=package_name,
+            editId=edit_id,
+            track=track,
+            body={'releases': [{'versionCodes': [version_code], 'status': 'draft'}]}
+        )
+
+        # version_code = 26
+        release_body = [{
+                'name':release_name,
+                'status':'draft',
+                'versionCodes':[version_code]
+                }]
+
+        track_request = service.edits().tracks().update(
+                packageName=package_name,
+                editId=edit_id,
+                track=track,
+                body={'releases': release_body}
+                )
+
+        response = track_request.execute()
+        print(f"Draft release '{release_name}' created with version code {version_code}")
+
+        # Commit the changes to finalize the edit
+        commit_request = service.edits().commit(
+            packageName=package_name,
+            editId=edit_id
+        )
+        commit_request.execute()
+
+        print("Changes committed and edit finalized.")
 
 
-def upload_assets(upload_url, name, data_path):
-    """Upload assets to a new release on GitHub."""
+class RelGithub:
 
-    github_token = get_github_token()
-    print(github_token)
+    def create_release(self, version, title, description, target_branch):
+        """Create a new release on GitHub."""
+        url = "https://api.github.com/repos/deku-messaging/Deku-SMS-Android/releases"
+        data = {
+            "tag_name": version,
+            "name": title,
+            "body": description,
+            "target_commitish": target_branch,
+            "draft": False,
+            "prerelease":False,
+            "generate_release_notes":False
+        }
 
-    headers = {'Content-Type': 'application/octet-stream', 
-               "Authorization": "Bearer {}".format(github_token), 
-               "X-GitHub-Api-Version": "2022-11-28",
-               "Accept": "application/vnd.github+json"}
+        github_token = get_github_token()
 
-    upload_url = re.sub(r"\{\?name,label}", "", upload_url)
+        print(github_token)
 
-    params = {
-        'name': name,
-        'label': name
-    }
+        headers = {"Authorization": "Bearer {}".format(github_token), 
+                   "X-GitHub-Api-Version": "2022-11-28",
+                   "Accept": "application/vnd.github+json"}
 
-    with open(data_path, 'rb') as f:
-        data = f.read()
+        response = requests.post(url, json=data, headers=headers)
+        response.raise_for_status()
 
-    response = requests.post(upload_url, headers=headers, data=data, params=params)
-    response.raise_for_status()
+        print(response.status_code)
+        return json.loads(response.text)
 
-    print(response.status_code)
-    return json.loads(response.text)
+    def get_github_token(self):
+        github_token = None
+        with open('release.properties', 'r') as fd:
+            lines = fd.readlines()
+
+        for line in lines:
+            if line.startswith('github_token'):
+                github_token = line.split("=")[1].strip() 
+                return github_token
+
+
+    def upload_assets(self, upload_url, name, data_path):
+        """Upload assets to a new release on GitHub."""
+
+        github_token = get_github_token()
+        print(github_token)
+
+        headers = {'Content-Type': 'application/octet-stream', 
+                   "Authorization": "Bearer {}".format(github_token), 
+                   "X-GitHub-Api-Version": "2022-11-28",
+                   "Accept": "application/vnd.github+json"}
+
+        upload_url = re.sub(r"\{\?name,label}", "", upload_url)
+
+        params = {
+            'name': name,
+            'label': name
+        }
+
+        with open(data_path, 'rb') as f:
+            data = f.read()
+
+        response = requests.post(upload_url, headers=headers, data=data, params=params)
+        response.raise_for_status()
+
+        print(response.status_code)
+        return json.loads(response.text)
 
 if __name__ == "__main__":
     """
@@ -77,6 +144,7 @@ if __name__ == "__main__":
     target_branch = 'dev'
     """
 
+    """
     version = sys.argv[1]
     title = sys.argv[2]
     description = sys.argv[3]
@@ -85,8 +153,17 @@ if __name__ == "__main__":
     data_path = sys.argv[6]
 
 
-    res = create_release(version, title, description, target_branch)
+    rel_github = RelGithub()
+    res = rel_github.create_release(version, title, description, target_branch)
     upload_url = res['upload_url']
     print(upload_url)
 
-    print(upload_assets(upload_url, name, data_path))
+    print(rel_github.upload_assets(upload_url, name, data_path))
+    """
+
+    rel_google = RelGooglePlaystore()
+    creds_file = sys.argv[1] 
+    release_id = 1
+    edit_body = "Testing new release"
+    bundle_file = sys.argv[2]
+    rel_google.create_edit_for_draft_release(creds_file, release_id, edit_body, bundle_file)
