2125f817663a3f85ba3eb436dc97a8017f4ecc13
sherlock wisdom <wisdomnji@gmail.com>
2023-09-18 20:32:23 +0100

    - fix: multi device support for linked devices

--- app/src/main/java/com/example/swob_deku/LinkedDevicesActivity.java
+++ app/src/main/java/com/example/swob_deku/LinkedDevicesActivity.java
@@ -99,7 +99,7 @@ public class LinkedDevicesActivity extends AppCompatActivity {
 
                             gatewayClient.setProjectBinding(bindings.get(0));
                             if(bindings.size() > 1)
-                                gatewayClient.setProjectBinding(bindings.get(1));
+                                gatewayClient.setProjectBinding2(bindings.get(1));
 
                             GatewayClientHandler gatewayClientHandler = new GatewayClientHandler(getApplicationContext());
                             long id = gatewayClientHandler.add(gatewayClient);
