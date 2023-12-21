67eb02dca5fbf7340d886a0d6e72c836ebb1a6f9
sherlock wisdom <wisdomnji@gmail.com>
2023-12-21 22:15:04 +0100

    - update: added language picker in settings. Need to work on the localization to include French

--- app/src/main/java/com/afkanerd/deku/DefaultSMS/Fragments/SettingsFragment.java
+++ app/src/main/java/com/afkanerd/deku/DefaultSMS/Fragments/SettingsFragment.java
@@ -1,10 +1,15 @@
 package com.afkanerd.deku.DefaultSMS.Fragments;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.app.LocaleManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.LocaleList;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
@@ -15,16 +20,33 @@ import com.afkanerd.deku.DefaultSMS.R;
 import com.afkanerd.deku.QueueListener.GatewayClients.GatewayClientListingActivity;
 import com.afkanerd.deku.Router.GatewayServers.GatewayServerListingActivity;
 
+import java.util.Locale;
+
 public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         setPreferencesFromResource(R.xml.settings_preferences, rootKey);
 
-        Preference securityPrivacyPreference = findPreference("settings_security_end_to_end");
+        Preference securityPrivacyPreference = findPreference(getString(R.string.settings_security_end_to_end));
         securityPrivacyPreference.setFragment(EndToEndFragments.class.getCanonicalName());
 
         Preference developersPreferences = findPreference(getString(R.string.settings_advanced_developers_key));
         developersPreferences.setFragment(DevelopersFragment.class.getCanonicalName());
+
+        ListPreference languagePreference = findPreference(getString(R.string.settings_locale));
+        languagePreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(@NonNull Preference preference, Object newValue) {
+                String languageLocale = (String) newValue;
+//                LocaleList currentAppLocales = getContext().getSystemService(LocaleManager.class)
+//                        .getApplicationLocales();
+
+                getContext().getSystemService(LocaleManager.class)
+                        .setApplicationLocales(
+                                new LocaleList(Locale.forLanguageTag(languageLocale)));
+                return true;
+            }
+        });
     }
 }
