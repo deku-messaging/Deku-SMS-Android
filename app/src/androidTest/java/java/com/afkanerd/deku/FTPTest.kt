4a1241b5637b206755a437527662b91de7091b11
sherlock <wisdomnji@gmail.com>
2024-04-01 18:54:53 +0800

    update: new test

--- app/src/androidTest/java/java/com/afkanerd/deku/FTPTest.kt
+++ app/src/androidTest/java/java/com/afkanerd/deku/FTPTest.kt
@@ -1,4 +1,17 @@
 package java.com.afkanerd.deku
 
+import androidx.test.filters.SmallTest
+
+@SmallTest
 class FTPTest {
+
+    @Before
+    fun init() {
+        context = InstrumentationRegistry.getInstrumentation().targetContext
+        val inputStream = context.resources.openRawResource(R.raw.smtp)
+        properties.load(inputStream)
+        properties.put("mail.smtp.host", properties.getProperty("host"))
+        properties.put("mail.smtp.port", properties.getProperty("port"))
+        properties.put("mail.debug", "true");
+    }
 }
