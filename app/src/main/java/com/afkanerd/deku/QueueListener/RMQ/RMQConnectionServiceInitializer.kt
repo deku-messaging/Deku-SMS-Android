1035fae8ed020a8b053d363ccdab06352e033ceb
sherlock <wisdomnji@gmail.com>
2024-06-02 22:24:55 +0100

    update: fixed issue with gateway server running with no gateway client

--- app/src/main/java/com/afkanerd/deku/QueueListener/RMQ/RMQConnectionServiceInitializer.kt
+++ app/src/main/java/com/afkanerd/deku/QueueListener/RMQ/RMQConnectionServiceInitializer.kt
@@ -4,13 +4,18 @@ import android.content.Context
 import android.content.Intent
 import androidx.startup.Initializer
 import androidx.work.WorkManagerInitializer
+import com.afkanerd.deku.Datastore
+import com.afkanerd.deku.Modules.ThreadingPoolExecutor
 import com.afkanerd.deku.NotificationsInitializer
 
 class RMQConnectionServiceInitializer : Initializer<Intent> {
     override fun create(context: Context): Intent {
         val intent = Intent(context, RMQConnectionService::class.java)
 
-        context.startForegroundService(intent)
+        ThreadingPoolExecutor.executorService.execute {
+            if(!Datastore.getDatastore(context).gatewayClientDAO()
+                .fetchActivated().isNullOrEmpty()) context.startForegroundService(intent)
+        }
 
         return intent
     }
