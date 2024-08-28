4971d23f15783cff589793dd9a7901b0ac41c26e
sherlock <wisdomnji@gmail.com>
2024-08-28 20:58:22 +0100

    - fix: published to maven -version bump

--- publish-maven.sh
+++ publish-maven.sh
@@ -1,4 +1,8 @@
-#!/usr/sh
+#!/bin/sh
 
+# https://vanniktech.github.io/gradle-maven-publish-plugin/central/#configuring-the-pom
+
+# Generate the secring file
+# gpg --keyring secring.gpg --export-secret-keys > ~/.gnupg/secring.gpg
 
 ./gradlew publishAndReleaseToMavenCentral --no-configuration-cache
