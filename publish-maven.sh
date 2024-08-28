320b7cffb750479b4effb8688cbab2d3e57fc11a
sherlock <wisdomnji@gmail.com>
2024-08-28 21:17:49 +0100

    update: modifications to pub docs

--- publish-maven.sh
+++ publish-maven.sh
@@ -2,6 +2,7 @@
 
 # https://vanniktech.github.io/gradle-maven-publish-plugin/central/#configuring-the-pom
 
+# KeyID = last 8 digits of public key 'gpg --list-keys'
 # Generate the secring file
 # gpg --keyring secring.gpg --export-secret-keys > ~/.gnupg/secring.gpg
 
